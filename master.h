d1da0e6d5dafbe4c1f3c091623b7f5539dc93070
celiadjeddou31-ux <celiadjeddou31@gmail.com>
2026-03-18 11:22:55 +0100

    Update master.h
    avecsha

--- master.h
+++ master.h
@@ -1,15 +1,13 @@
 #ifndef MASTER_H
 #define MASTER_H
+
+#include "sha256.h"
+#include "aes.h"
 #define MASTER_FILE ".master"
-#define HASH_SIZE 32
-
-typedef struct {
-    unsigned char hash[HASH_SIZE];
-} MasterKey;
-
-extern int master_exists(const char *filepath);
-extern void create_master(const char *filepath);
-extern int load_master(const char *filepath, unsigned char *hash);
-extern int verify_master(const char *filepath);
+int master_exists(const char *filepath);
+void create_master(const char *filepath);
+int load_master(const char *filepath, unsigned char *hash);
+int verify_master(const char *filepath);
+void derive_key(const char *password, unsigned char key[AES_KEY_SIZE]);
 
 #endif
